1075a8851858113f33d9b8241d1c487bd8afc73a
thanhle <mix2pigcat@gmaail.com>
2019-09-26 13:27:38 +0700

    filt html

---
+++ nhungbai1.js
@@ -0,0 +1,7 @@
+let width, height, area;
+width= prompt("nhap chieu rong: ");
+height = prompt("nhap chieu cao: ");
+width = parseInt(width);
+height = parseInt(height);
+area = width * height;
+document.write("area="+area);
